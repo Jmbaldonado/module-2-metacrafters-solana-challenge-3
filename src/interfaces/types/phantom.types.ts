e1f653bd059f748faf1d937edbea54de2d80f7c2
JM Baldonado <john.baldonado@xurpas.com>
2023-01-15 22:10:48 +0800

    Finish up challenge, add disconnect button

---
+++ src/interfaces/types/phantom.types.ts
@@ -0,0 +1,10 @@
+type DisplayEncoding = "utf8" | "hex";
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+type PhantomRequestMethod =
+  | "connect"
+  | "disconnect"
+  | "signTransaction"
+  | "signAllTransactions"
+  | "signMessage";
+
+export type { DisplayEncoding, PhantomEvent, PhantomRequestMethod };
